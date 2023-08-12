a62b0f3e3310a7328b4dc55d346bceafc615092a
Георги Сталев <sttallev99@gmail.com>
2023-08-12 19:53:22 +0300

    create Announcement and slider components.

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://i.insider.com/631a4970c3104e0012502d84?width=700',
+        title: 'SUMMER SALE',
+        desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: 'https://i.insider.com/631a4970c3104e0012502d84?width=700',
+        title: 'WINTER SALE',
+        desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'fcf1ed'
+    },
+    {
+        id: 3,
+        img: 'https://i.insider.com/631a4970c3104e0012502d84?width=700',
+        title: 'POPULAR ITEMS',
+        desc: 'DON\'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.',
+        bg: 'fbf0f4'
+    }
+];
